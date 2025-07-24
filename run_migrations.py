35cb1f7518f86203684ad5615174a2c083887418
Vibhore Mathur <onlyvibhore@gmail.com>
2025-07-25 00:28:03 +0530

    migrate

--- run_migrations.py
+++ run_migrations.py
@@ -4,6 +4,11 @@ from flask_migrate import upgrade
 from app.Models import db
 from app.config.config import Config
 
+from app.Models.user import User
+from app.Models.product import Product
+from app.Models.cart import Cart, CartItem
+from app.Models.order import Order
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
